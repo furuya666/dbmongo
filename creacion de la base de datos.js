4330b995ff12d2dddbb919e01c42f39e7869ab3d
furuya666 <furuyagary@gmail.com>
2020-04-19 12:54:24 -0400

    Agregando el scrip de la shell de la base de datos

--- creacion de la base de datos.js
+++ creacion de la base de datos.js
@@ -15,20 +15,20 @@ db.createUser(
     }
 );
 db.niño.insert([
-    { _id: "7460", Nombre: "Gina", Apellido_Paterno: "Greenholt", Apellido_Materno: "Mamani",Edad:5,Estatura:1.30,Peso:40},
-    { _id: "5187", Nombre: "Judah", Apellido_Paterno: "Runolfsson", Apellido_Materno: "Quispe",Edad:6,Estatura:1.30,Peso:35 },
-    { _id: "3018", Nombre: "Kristofer", Apellido_Paterno: "Schmeler", Apellido_Materno: "Condori",Edad:4,Estatura:1.28,Peso:30 },
-    { _id: "0594", Nombre: "Gabriella", Apellido_Paterno: "Abernathy", Apellido_Materno: "Cortez" ,Edad:3,Estatura:1.20,Peso:30}
+    { _id: "1", Nombre: "Gina", Apellido_Paterno: "Greenholt", Apellido_Materno: "Mamani",Edad:5,Estatura:1.30,Peso:40},
+    { _id: "2", Nombre: "Judah", Apellido_Paterno: "Runolfsson", Apellido_Materno: "Quispe",Edad:6,Estatura:1.30,Peso:35 },
+    { _id: "3", Nombre: "Kristofer", Apellido_Paterno: "Schmeler", Apellido_Materno: "Condori",Edad:4,Estatura:1.28,Peso:30 },
+    { _id: "4", Nombre: "Gabriela", Apellido_Paterno: "Abernathy", Apellido_Materno: "Cortez" ,Edad:3,Estatura:1.20,Peso:30}
    
   
 ]);
 db.padre_adoptivo.insert([
-    { _id: "7461", Nombre_Padre: "Marcos", Apellido_P_Padre: "Greenholt", Apellido_M_Padre: "Mamani", CI_Padre:"111111",Nombre_Madre: "Maria", Apellido_P_Madre: "Greenholt", Apellido_M_Madre: "Mamani", CI_Madre:"11111100"},
-    { _id: "518777", Nombre_Padre: "Kevin", Apellido_P_Padre: "Runolfsson", Apellido_M_Padre: "Quispe", CI_Padre:"112222",Nombre_Madre: "Martha", Apellido_P_Madre: "Gomez", Apellido_M_Madre: "Mamani", CI_Madre:"111111999" },
-    { _id: "301855", Nombre_Padre: "Carlos", Apellido_P_Padre: "Schmeler", Apellido_M_Padre: "Condori", CI_Padre:"1223332",Nombre_Madre: "Marcela", Apellido_P_Madre: "Velarde", Apellido_M_Madre: "Mamani", CI_Madre:"122222" },
-    { _id: "059433", Nombre_Padre: "Raul", Apellido_P_Padre: "Abernathy", Apellido_M_Padre: "Cortez" , CI_Padre:"1114444",Nombre_Madre: "Gabriela", Apellido_P_Madre: "Greenholt", Apellido_M_Madre: "Mamani", CI_Madre:"3333333"}
+    { _id: "1", Nombre_Padre: "Marcos", Apellido_P_Padre: "Greenholt", Apellido_M_Padre: "Mamani", CI_Padre:"111111",Nombre_Madre: "Maria", Apellido_P_Madre: "Greenholt", Apellido_M_Madre: "Mamani", CI_Madre:"11111100"},
+    { _id: "2", Nombre_Padre: "Kevin", Apellido_P_Padre: "Runolfsson", Apellido_M_Padre: "Quispe", CI_Padre:"112222",Nombre_Madre: "Martha", Apellido_P_Madre: "Gomez", Apellido_M_Madre: "Mamani", CI_Madre:"111111999" },
+    { _id: "3", Nombre_Padre: "Carlos", Apellido_P_Padre: "Schmeler", Apellido_M_Padre: "Condori", CI_Padre:"1223332",Nombre_Madre: "Marcela", Apellido_P_Madre: "Velarde", Apellido_M_Madre: "Mamani", CI_Madre:"122222" },
+    { _id: "4", Nombre_Padre: "Raul", Apellido_P_Padre: "Abernathy", Apellido_M_Padre: "Cortez" , CI_Padre:"1114444",Nombre_Madre: "Gabriela", Apellido_P_Madre: "Greenholt", Apellido_M_Madre: "Mamani", CI_Madre:"3333333"}
 ]);
-db.cita_programada.insert( { _id: "913", niño: "7460",padre_adoptivo:"7461" });
+db.cita_programada.insert( { _id: "1", niño: "1",padre_adoptivo:"2" });
 
-db.cita_programada.insert( { _id: "883",niño: "5187",padre_adoptivo:"059433" } );
+db.cita_programada.insert( { _id: "2",niño: "3",padre_adoptivo:"4" } );
 
--- queries.js
+++ queries.js
@@ -1,4 +1,18 @@
+﻿//Creando mis collections
+db.createCollection("niño");
+db.createCollection("padre_adoptivo");
+db.createCollection("cita_programada");
+//Para hacer un listado de mis documnetos de mis collections de mi base de datos
 db.niño.find();
 db.cita_programada.find();
 db.padre_adoptivo.find();
+//Para modificar los datos de los niños 
+//Por ejemplo modificando la edad del niño con _id=2
+db.niño.update({_id:"2"},{$set:{Edad:6}});
+//Para eliminar de la lista aun niño ya adoptado
+//En este caso se eliminra al niño con el  _id=1
+db.niño.deleteOne({_id:"2"});
+
+
+
 
