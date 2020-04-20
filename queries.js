0704c02ea5f4d37205db7038416e62930e3b560b
furuya666 <furuyagary@gmail.com>
2020-04-20 10:57:39 -0400

    Agregando los queries a mi base de datos

--- queries.js
+++ queries.js
@@ -12,6 +12,12 @@ db.niño.update({_id:"2"},{$set:{Edad:6}});
 //Para eliminar de la lista aun niño ya adoptado
 //En este caso se eliminra al niño con el  _id=1
 db.niño.deleteOne({_id:"2"});
+//Ordenar la lista de niños por id de forma ascendente
+db.niño.find().sort({_id:1});
+//Mostrando a un niños especifico por su id
+db.niño.find({_id:"1"});
+//Listando niños por Nombre y edad
+db.niño.find({},{Nombre:1,Edad:1});
 
 
 
